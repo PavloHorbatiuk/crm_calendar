3701cb62d3f875d71a60b0817b7bf9067fa11d21
Pavlo Horbatiuk <pavlohorbatiuk@gmail.com>
2024-05-04 13:15:35 +0300

    add: dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,24 @@
+import { useEventStore } from '@/store/eventStore';
 import { CardTitle } from '../ui/CardTitle/CardTitle';
 import { CardWrapper } from '../ui/CardWrapper/CardWrapper';
+import { useShallow } from 'zustand/react/shallow';
+import EventChart from '../EventsChart/EventsChart';
 
 function Dashboard() {
+  const events = useEventStore(useShallow((state) => state.events));
   return (
     <>
       <CardTitle>
         <h4>Dashboard</h4>
       </CardTitle>
-      <CardWrapper>Body</CardWrapper>
+      <CardWrapper>
+        <div className="h-1/2 flex">
+          <div className="max-w-[18rem] w-full p-4">To day</div>
+          <div className="h-full flex-1">
+            <EventChart events={events} />
+          </div>
+        </div>
+      </CardWrapper>
     </>
   );
 }
