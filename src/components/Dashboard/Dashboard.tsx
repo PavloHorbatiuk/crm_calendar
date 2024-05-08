23a3e4f7f527139b6ad2dae7e19d5b2d27dc7370
Pavlo Horbatiuk <pavlohorbatiuk@gmail.com>
2024-05-08 16:10:55 +0300

    add: change color

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -13,7 +13,7 @@ function Dashboard() {
       <div className="h-full">
         <div className="h-1/2 flex-auto flex gap-1 ">
           <div className="max-w-[18rem] shadow min-w-44 w-full p-4 bg-white rounded-3xl">
-            <div className="flex justify-between items-center p-2 bg-lightGray shadow rounded-3xl">
+            <div className="flex justify-between items-center p-2 bg-blueMoon shadow rounded-3xl">
               <div className="inline-block rounded min-h-[1em] w-2 self-stretch bg-red300"></div>
               <span className="text-sm">Angelina 13-00</span>
               <div className="flex items-center">
