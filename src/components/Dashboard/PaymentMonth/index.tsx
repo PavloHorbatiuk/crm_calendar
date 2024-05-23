c185f145a496b47ba9754233493d5e4a38cefe82
texasjkee <gt.gluck@gmail.com>
2024-05-23 12:08:03 +0300

    delete: month component

--- src/components/Dashboard/PaymentMonth/index.tsx
+++ src/components/Dashboard/PaymentMonth/index.tsx
@@ -1,10 +1,9 @@
-import { daysNames } from '@/common/const/fullDateNames';
-import { getDaysInMonth } from 'date-fns';
+import { useState } from 'react';
+import { daysNames, monthNames } from '@/common/const/fullDateNames';
 import { getArrayOfDays } from '@/utils/getArrayOfDays';
 import { type Event } from '@/store/eventStore/types';
 
 import PaymentMonthEvent from './Item';
-import Month from './Month';
 
 interface PaymentMonthProps {
   success: boolean;
@@ -13,16 +12,43 @@ interface PaymentMonthProps {
 }
 
 const gray = 'bg-gray';
+const getPrevMonth = (d: Date) => new Date(d.getFullYear(), d.getMonth() - 1);
+const getNextMonth = (d: Date) => new Date(d.getFullYear(), d.getMonth() + 1);
 
 function PaymentMonth({ monthEvents, onUpdate, success }: PaymentMonthProps) {
-  const daysOfMonth = getArrayOfDays(getDaysInMonth(new Date()));
+  const [month, setMonth] = useState(new Date());
+
+  const daysOfMonth = getArrayOfDays(month);
   const daysOfWeek = getWeekStartingMonday();
   const daysWithEvents = getDaysWithEvents(monthEvents);
+
+  const onClick = (event: any) => {
+    const prev = event.target.dataset['prev'];
+    prev ? setMonth(getPrevMonth(month)) : setMonth(getNextMonth(month));
+  };
 
   return (
     <div className="h-1/2 mt-1 w-full p-4 bg-white rounded-3xl shadow">
       <div className="flex justify-center items-center text-md mb-1">
-        <Month />
+        <div className="flex w-40">
+          <button
+            className="mr-2 bg-slate-400"
+            data-prev="prev"
+            onClick={onClick}
+          >
+            left
+          </button>
+          <button
+            className="ml-2 bg-slate-400"
+            data-next="next"
+            onClick={onClick}
+          >
+            right
+          </button>
+          <div>
+            <div className="w-22 ml-4">{monthNames[month.getMonth()]}</div>
+          </div>
+        </div>
       </div>
       <div className="flex gap-28 px-20">
         {daysOfWeek.map((day, dayIndex) => (
--- src/utils/getArrayOfDays.ts
+++ src/utils/getArrayOfDays.ts
@@ -1,11 +1,17 @@
-export function getArrayOfDays(amountOfDays: number) {
-  const year = new Date().getFullYear();
-  const month = new Date().getMonth();
-  const dayOfWeek = new Date(year, month, 1).getDay() - 2;
+import { getDaysInMonth } from 'date-fns';
+
+export function getArrayOfDays(_month: Date) {
+  const amountOfDays = getDaysInMonth(_month);
+  const year = new Date(_month).getFullYear();
+  const month = new Date(_month).getMonth();
+  const dayOfWeek = new Date(year, month, 1).getDay();
 
   const lastDaysOfMonth = new Array(dayOfWeek).fill(0);
   const currentDaysOfMonth = [...Array(amountOfDays).keys()];
+  currentDaysOfMonth.shift();
   currentDaysOfMonth.push(amountOfDays);
+  const arr = [...lastDaysOfMonth, ...currentDaysOfMonth];
+  arr.shift();
 
-  return [...lastDaysOfMonth, ...currentDaysOfMonth];
+  return arr;
 }
