7b2cd8a829c10d44a1556cd25fd81e034dab093b
texasjkee <gt.gluck@gmail.com>
2024-07-14 11:43:52 +0300

    fix: fetch in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import { RouterProvider } from '@tanstack/react-router';
 import { useAuth } from './common/hooks/useAuth';
 import { router } from './routes/___route.config';
-import { useEventStore } from './store/eventStore';
-import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
   const authentication = useAuth();
-  const { getAllEvents } = useEventStore();
-
-  useEffect(() => {
-    getAllEvents();
-  }, [getAllEvents]);
 
   return (
     <div className="bg-lightBlue h-screen p-5 max-sm:p-1">
